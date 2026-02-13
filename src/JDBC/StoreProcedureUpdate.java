9cb16d7764eb8093ad3aa4373a80bacd1afaa827
shrus <shrushtibhade2004@gmail.com>
2026-02-13 22:32:22 +0530

    jdbc

---
+++ src/JDBC/StoreProcedureUpdate.java
@@ -0,0 +1,55 @@
+package JDBC;
+
+import java.sql.CallableStatement;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.util.Scanner;
+
+public class StoreProcedureUpdate {
+    static void main() {
+        String url = "jdbc:mysql://localhost:3306/bank_class";
+        String user = "root";
+        String password = "Srushti@123";
+
+        Scanner sc = new Scanner(System.in);
+        Connection con = null;
+        CallableStatement cst = null;
+        //   ResultSet rs = null;
+
+        String sql = "call data_update(?)";
+
+        try {
+            Class.forName("com.mysql.cj.jdbc.Driver");
+            System.out.println("Driver loaded Successfully");
+
+            con = DriverManager.getConnection(url, user, password);
+            System.out.println("Connection Established");
+
+            cst = con.prepareCall(sql);
+
+
+            System.out.println("enter balance");
+            int balance = sc.nextInt();
+
+
+            cst.setInt(1, balance);
+
+            cst.execute();
+
+
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        } finally {
+            try {
+                //       rs.close();
+                cst.close();
+                con.close();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
+
+
